2dfcb87fd79132fa16f6928ce70099e96988942d
Benedek Sumicz <benedek.sumicz@stud.uni-corvinus.hu>
2021-10-01 22:54:31 +0200

    Delete

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -22,6 +22,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.Write;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -54,6 +55,13 @@ namespace UserMaintenance
                     sw.WriteLine();
                 }
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var törlés = listBox1.SelectedItem;
+            if (törlés != null)
+                users.Remove((User)törlés);
         }
     }
 }
