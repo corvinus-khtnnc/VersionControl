c412bf88dcb7ef0aa7c379c7962a4e0bf87ef7f9
Benedek Sumicz <benedek.sumicz@stud.uni-corvinus.hu>
2021-10-14 18:44:15 +0200

    Add Excel namespace

--- ExcelExport/ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/ExcelExport/Form1.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelExport
 {
     public partial class Form1 : Form
-    {
+    { 
+        
         RealEstateEntities context = new RealEstateEntities();        
         List<Flat> lakasok;
         
